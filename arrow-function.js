fce36e4f9dc3ea591feae42916828f991ec7d100
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-26 23:35:28 +0530

    add

---
+++ arrow-function.js
@@ -0,0 +1,8 @@
+const addition = (a, b) => {
+    return a + b;
+};
+const  result = addition(10, 5);
+const  addition2 = () => "Hello, World!";
+const message = addition2();
+console.log("Result===>", result);
+console.log("Message===>", message);
