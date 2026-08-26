c93e26fa5189d0fa0f48bdee9c68103fc04dd4c2
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-26 17:48:12 +0530

    add

---
+++ comparisionOperator.js
@@ -0,0 +1,9 @@
+const a=100;
+const b=50;
+
+console.log( "Is a is greater than :", a > b);
+console.log( "Is a is less than :", a < b);
+console.log( "Is a is equal to :", a == b);
+console.log( "Is a is not equal to :", a != b); 
+console.log( "Is a is greater than or equal to :", a >= b);
+console.log( "Is a is less than or equal to :", a <= b);  
