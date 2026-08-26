e158314db119d0f4f0263349d16abaa01472f1b2
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-26 11:04:14 +0530

    add

---
+++ assginmentOperstor.js
@@ -0,0 +1,16 @@
+let a = 100;
+
+a +=50;
+console.log("After Addition :", a);
+
+a -=20;
+console.loh("After Subration:", a);
+
+a *=2;
+console.log("After Multiplication:", a);
+
+a /=5;
+console.log("After Division:", a);
+
+a %= 3;
+console.log("After Modulus :", a); 
