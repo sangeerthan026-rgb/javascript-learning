f7362b30f7c5892fa1537e6c25633501ea10a563
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-26 18:38:02 +0530

    add nullish operator demonstration in nullishoperator.js

---
+++ nullishoperator.js
@@ -0,0 +1,6 @@
+
+var defaultCm =  "msv";
+
+console.log("candidate Name ====>",defaultCm)
+console.log("candidate Name length====>",defaultCm.length)
+console.log("candidate Name uppercase====>",defaultCm.toUpperCase())
