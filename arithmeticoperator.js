94c4f90354e6595c0b7aafbd00c0093107071eb2
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-26 10:46:44 +0530

    add

---
+++ arithmeticoperator.js
@@ -0,0 +1,8 @@
+const a=20;
+const b=10;
+
+console.log("addition :",a+b);
+console.log("subraction:",a-b);
+console.log("multiplication:",a*b);
+console.log("division:",a/b);
+console.log("modules:",a%b);
