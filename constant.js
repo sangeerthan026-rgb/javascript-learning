918016a25c3bacdb00e0c1b1d788ef93b6190afa
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-26 17:50:24 +0530

    add constants and logging for bottle, plasticCover, dbUrl, and paymentUrl

---
+++ constant.js
@@ -0,0 +1,14 @@
+const bottle = "oil";
+
+
+const plasticCover = "chilliPowder"
+
+
+
+const dbUrl = "mysql://localhost:3306/mydb";
+const paymentUrl = "razorpay:9123@abl.com"
+console.log("bottle===>", bottle);
+
+console.log("plasticCover===>", plasticCover);
+console.log("dbUrl===>", typeof dbUrl);
+console.log("paymentUrl===>", typeof paymentUrl);
