cf8bb44b9aed7ee24d1a5b74bd01d3e1ff057648
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-26 23:20:36 +0530

    add

---
+++ elseifLader.js
@@ -0,0 +1,17 @@
+let mark =91;
+
+if (mark >=90){
+    console.log("Grade A")
+}
+else if (mark >=80 && mark<90){
+    console.log("Grade B")
+}
+else if (mark >= 70 && marks < 80){
+    console.log("Grade C")
+}
+else if (marks >= 40 && marks < 70){
+    console.log("Grade D")
+}
+else{
+    console.log("Grade F")
+}
