4a7d909a9c0cbf9938d38ecd98bcb9a6e347ddce
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-26 18:17:48 +0530

    add initial implementation of arithmetic operations in implict.js

---
+++ implict.js
@@ -0,0 +1,10 @@
+
+
+var firstNumber = "100"
+var secondNumber= 50
+
+var additionalResult = FirstNumber + secondNumber;
+var subractionResult = firstNumber - secondNumber; 
+
+console.log(additionResult)
+console.log(subractionResult)
