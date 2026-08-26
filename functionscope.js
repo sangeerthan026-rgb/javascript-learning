97a3e6565bfd54a08aa4a9bf7bcf444743019790
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-26 23:24:40 +0530

    add

---
+++ functionscope.js
@@ -0,0 +1,5 @@
+var bottle = "oil"                           // declare
+var bottle = "water"                         // re-declare
+var bottle = "juice"
+var bottle = "curd"
+console.log("bottle====>",bottle)
