26871b14d515177dfdec1b3a32bc59a18502b00b
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-26 10:55:54 +0530

    add

---
+++ array.js
@@ -0,0 +1,7 @@
+let fruits = ["apple","orange","grapes","pinaple","lichi"]
+let salaryBasic = [5000,6000,7000,9000,8000]
+let personDetails = ["sangeeth",22,"BE",20000.25,"Zoho"]
+let vegtable = ["onion","cabbage","brinjal","cucumber"]
+
+
+console.log("personDetails ===>",personDetails[3])
