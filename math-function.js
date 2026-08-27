28f1f44ce4049129abb889b9d3f82a0a917b5131
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-27 22:44:02 +0530

    add: implement math functions for rounding, flooring, and random number generation

---
+++ math-function.js
@@ -0,0 +1,15 @@
+let apple = 20.25;
+
+let round = Math.round(apple);
+let cell  = Math.cell(apple);
+let floor = Math.floor(apple)
+let random  = Math.random()
+let otp     = Math.round(Math.random()*9000)
+
+
+console.log(apple);
+console.log("round====>",round)
+console.log("ceil====>",ceil)
+console.log("floor====>",floor)
+console.log("random====>",random)
+console.log("otp====>",otp)
