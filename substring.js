1e2193dc95b756dbe933038f30c85167aa73a5e7
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-27 22:56:01 +0530

    add

---
+++ substring.js
@@ -0,0 +1,9 @@
+let str = " hello world ";
+
+let sliceStr = str.slice(0, 5);
+let substringStr = str.substring(6, 11);
+let substrStr = str.substr(3, 5);
+
+console.log(sliceStr); 
+console.log(substringStr); 
+console.log(substrStr); 
