ceb059460f043ad774b3d520bf406dfa93178296
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-27 22:14:19 +0530

    add: implement data getter functions for current date and time logging

---
+++ data-getterfn.js
@@ -0,0 +1,11 @@
+let timeStamp      =  new Date();
+let year           =  timeStamp.getFullYear()
+let month          =  timeStamp.getMonth() + 1
+let currentDate    =  timeStamp.getDate()
+let day            =  timeStamp.getDay()
+
+console.log("timeStamp===>",timeStamp)
+console.log("year===>",year)
+console.log("month===>",month)
+console.log("currentDate===>",currentDate)
+console.log("day===>",day)
