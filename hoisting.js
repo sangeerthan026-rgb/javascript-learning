31b6b1241048fb7ffe5c5ef865d28839126ed58d
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-27 18:34:51 +0530

    add hoisting example with function declaration and console logs

---
+++ hoisting.js
@@ -0,0 +1,9 @@
+console.log("hello");
+console.log("i am learning JavaScript");
+let name = "John";
+console.log("My name is " + name);
+// finally calling the function after its declaration
+hello();
+function hello() {
+    console.log("Hello from function");
+}
