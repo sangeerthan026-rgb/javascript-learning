91357a72a6ed7f78d07587d6ba377dc29d66454a
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-27 22:10:23 +0530

    add: implement string manipulation functions with logging

---
+++ chain-function.js
@@ -0,0 +1,8 @@
+let str = "hari is a mernstack developer";
+let newstr = str.charAt(15).toUpperCase()+" " +str.slice(0,6);
+
+let reversestr = str.splite(" ").reverse(). join
+("");
+console.log(str);
+console.log(newstr);
+console.log(reversestr);
