d2eae04cdae3457d3e1a9e7ac4945ce6dbf2e12e
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-28 11:17:51 +0530

    add: implement filtering for values greater than 500 and unique values in an array

---
+++ filter.js
@@ -0,0 +1,17 @@
+let orignalArr =["100","200","300","400","500","650"]
+let greaterthen500 = orignalArr.filter(
+    (value,index,arr)=>{
+        return value >500
+
+    }
+)
+console.log(greaterthen500)
+
+let array = [5,5,5,5,100,50,4,4,9,6]
+let uniqueArr = array.filter(
+    (value,index,arr)=>{
+        reurn arr.indexOf(value)==index
+
+    }
+)
+console.log(uniqueArr)
