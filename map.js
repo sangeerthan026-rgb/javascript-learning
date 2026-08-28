6e0ad356aa58ce16be078acf35ed4ee9d37126fe
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-28 13:06:03 +0530

    add: implement map method to create a duplicate and multiplied array

---
+++ map.js
@@ -0,0 +1,17 @@
+let orignalArr = [100,200,300,400,750]
+
+let duplicateArr = orignalArr.map(
+    (value,index,arr) =>{
+        return value
+    }
+)
+let  newArrmulfive = orignalArr.map(
+    (value,index,arr)=>{
+        return value*5
+
+    }
+)
+
+console.log(orignalArr)
+console.log(duplicateArr)
+console.log(newArrmulfive)
