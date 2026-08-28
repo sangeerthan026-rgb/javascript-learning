9c62710e91b0d1c44d5829709bd71cee89fca3cd
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-28 10:45:02 +0530

    add: implement string conversion and special character removal for fruits

---
+++ convertstring.js
@@ -0,0 +1,6 @@
+let fruits=["apple","orange","grapes","pineapple","mango"]
+
+let arrtostring = fruits.toString()
+let removeSplchar = fruits.join("*")
+
+console.log("removeSplchar===.",removeSplchar)
