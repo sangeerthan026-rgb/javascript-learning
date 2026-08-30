48fcc4e94d099eab637192e049ac947b4c38c850
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-30 15:25:53 +0530

    delet

--- event-handling/index.js
+++
@@ -1,8 +0,0 @@
-function buttonOneEvent(){
-    console.log("button1 is clicked")
-}
-
-const buttonElement = document.getElementById("button2")
-buttonElement.addEventListener("click",()=>{
-    console.log("Button2 is clicked")
-})
--- selecting-element/index.js
+++
@@ -1,2 +0,0 @@
-const element1 = document.getElementById("content1")
-console.log("element1===>",element1)
