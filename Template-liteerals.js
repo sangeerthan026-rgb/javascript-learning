7766fa497711a3562e85f9b09d89d476d1ebe72e
sangeerthan026 <sangeerthan026@gmail.com>
2026-08-30 15:26:46 +0530

    lite

---
+++ Template-liteerals.js
@@ -0,0 +1,17 @@
+const firstName = `Rajesh`
+const lastName  = `Kumar`
+const desigination  = `Engineer`
+
+const employee = `Employee name is ${firstName} ${lastName}
+    he is a ${desigination}
+   currently working from prashanth hospiltal `
+
+console.log(employee)
+
+
+const  a = 100
+const  b = 50
+
+console.log(`a====>${a}
+             b====>${b}
+             add ===> ${a + b}`)
